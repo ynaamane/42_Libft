f376518d81fec3ccf63a08e96b1772a5aad952f8
Yanis NAAMANE <ynaamane@e3r2p8.42.fr>
2018-11-16 18:41:45 +0100

    Adding ft_isprint.c ft_tolower.c ft_toupper.c and updating libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ynaamane <ynaamane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 11:01:52 by ynaamane          #+#    #+#             */
-/*   Updated: 2018/11/16 16:55:10 by ynaamane         ###   ########.fr       */
+/*   Updated: 2018/11/16 18:39:23 by ynaamane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,5 +39,8 @@ int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
 int		ft_isascii(int c);
+int		ft_isprint(int c);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
 
 #endif
