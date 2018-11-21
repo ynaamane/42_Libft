c20d21c501a4f1b2a0b31b3567a79a02ec956a82
Yanis NAAMANE <ynaamane@e3r2p9.42.fr>
2018-11-21 13:16:39 +0100

    Adding ft_strsplit.c and Updating libft.h

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,38 +6,70 @@
 /*   By: ynaamane <ynaamane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/20 13:33:14 by ynaamane          #+#    #+#             */
-/*   Updated: 2018/11/20 17:28:19 by ynaamane         ###   ########.fr       */
+/*   Updated: 2018/11/21 13:16:08 by ynaamane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
-char	**ft_strsplit(char const *s, char c)
+static int	ft_nbwords(char *s, char c)
 {
 	int		i;
-	int		i2;
-	int		i3;
-	int		*count;
-	char	*s1;
-	int 	**result;
+	int		is_word;
 
 	i = 0;
-	i2 = 0;
-	s1 = s;
-	result = 0;
-	if (s == NULL || c == NULL)
-		return (NULL);
-	while (s[i] != '\0')
+	is_word = 0;
+	while (*s)
 	{
-		i2 = 0;
-		if (s1[i] = c)
+		if (is_word == 0 && *s != c)
 		{
-			while (s1[i2] != c)
-			strcpy(result[count[i3]], s1[i2]);
-			i2++;
+			is_word = 1;
+			i++;
 		}
+		else if (is_word == 1 && *s == c)
+			is_word = 0;
+		s++;
+	}
+	return (i);
+}
+
+static int	ft_strlenlim(char *s, char c)
+{
+	int		len;
+
+	len = 0;
+	while (*s != c && *s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char		**ft_strsplit(char const *s, char c)
+{
+	int		nb_ofwords;
+	char	**tab;
+	int		i;
+
+	if (!s)
+		return (NULL);
+	nb_ofwords = ft_nbwords((char )*s, c);
+	tab = (char **)malloc((nb_ofwords + 1) * sizeof(char*));
+	i = 0;
+	if (!tab)
+		return (NULL);
+	while (nb_ofwords--)
+	{
+		while (*s == c && *s != '\0')
+			s++;
+		tab[i] = ft_strsub((char *)s, 0, ft_strlenlim((char *)s, c));
+		if (!tab[i])
+			return (NULL);
+		s = s + ft_strlenlim((char *)s, c);
 		i++;
 	}
-	return (**result);
+	tab[i] = NULL;
+	return (tab);
 }
