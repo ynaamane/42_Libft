ff2533a5a0b59626fb556db35712ee7fbc95a870
Yanis NAAMANE <ynaamane@e3r2p9.42.fr>
2018-11-30 12:18:35 +0100

    Bug fix

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: ynaamane <ynaamane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 13:17:22 by ynaamane          #+#    #+#             */
-/*   Updated: 2018/11/30 11:05:36 by ynaamane         ###   ########.fr       */
+/*   Updated: 2018/11/30 12:17:53 by ynaamane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 
 	if (src == NULL || dst == NULL)
 		return (NULL);
-	if (!(cpy = (char*)malloc(sizeof(char*) * len)))
+	if (!(cpy = (char*)malloc(sizeof(char) * len)))
 		return (NULL);
 	cpy = ft_strncpy(cpy, src, len);
 	dst = (void*)ft_strncpy(dst, cpy, len);
